0b555480a168da6505f3807bcd339a4d8e66b967
gaeun <gaeundev@gmail.com>
2023-04-07 11:50:08 +0900

    [setting] 스토리북 v7 설치 및 설정

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+// Replace your-framework with the framework you are using (e.g., react-webpack5, vue3-webpack5)
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
